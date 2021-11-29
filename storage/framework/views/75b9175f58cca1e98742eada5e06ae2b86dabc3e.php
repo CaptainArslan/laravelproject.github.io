5a001069ed8fdd939e86bf4c2466fab5cadc20ff
Muhammad Arslan <mughalarslan996@gmail.com>
2021-11-29 15:33:38 +0500

    storage

---
+++ storage/framework/views/75b9175f58cca1e98742eada5e06ae2b86dabc3e.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Laravel Crud Form</title>
+
+<!-- <link rel="stylesheet" href="../css/style.css"> -->
+<link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="<?php echo e(asset('js/jqueryformvaldator.js')); ?>"></script>
+
+<script>
+$(document).ready(function(){
+	// Activate tooltip
+	$('[data-toggle="tooltip"]').tooltip();
+	
+	// Select/Deselect checkboxes
+	var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		if(this.checked){
+			checkbox.each(function(){
+				this.checked = true;                        
+			});
+		} else{
+			checkbox.each(function(){
+				this.checked = false;                        
+			});
+		} 
+	});
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
+});
+
+</script>
+
+
+</head>
+<body><div class="container-xl">
+	<div class="table-responsive">
+		<div class="table-wrapper">
+			<div class="table-title">
+				<div class="row">
+					<div class="col-sm-6">
+						<h2>Manage <b>Employees</b></h2>
+					</div>
+					<div class="col-sm-6">
+						<a href="" class="btn btn-success" id="add_user" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+						<a href="" class="btn btn-danger" id="delete_multiple_user" data-toggle="modal"><i class="material-icons" onclick=" return jqisChecked(); ">&#xE15C;</i> <span>Delete Selected</span></a>						
+					</div>
+				</div>
+			</div>
+			<table class="table table-striped table-hover ">
+				<thead >
+					<tr>
+						<th>
+							<span class="custom-checkbox">
+								<input type="checkbox" id="selectAll">
+								<label for="selectAll"></label>
+							</span>
+						</th>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Address</th>
+						<th>Phone</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $__currentLoopData = $user; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $db_users): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<tr>
+						<td>
+							<span class="custom-checkbox">
+								<input type="checkbox" id="checkbox1" name="options[]" value="1">
+								<label for="checkbox1"></label>
+							</span>
+						</td>
+						<td><?php echo e($db_users->id); ?></td>
+						<td><?php echo e($db_users->user_firstname); ?></td>
+						<td><?php echo e($db_users->user_email); ?></td>
+						<td><?php echo e($db_users->user_address); ?></td>
+						<td><?php echo e($db_users->user_phone); ?></td>
+						<td>
+							<a href="<?php echo e($db_users->id); ?>" class="edit_user" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+ 							<a href="<?php echo e($db_users->id); ?>" class="delete" data-toggle="modal"  ><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+						</td>
+					</tr>				
+					<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+				</tbody>
+			</table>
+			<div class="clearfix">
+				<div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+				<ul class="pagination">
+					<li class="page-item disabled"><a href="#">Previous</a></li>
+					<li class="page-item active"><a href="#" class="page-link">1</a></li>
+					<li class="page-item"><a href="#" class="page-link">2</a></li>
+					<li class="page-item "><a href="#" class="page-link">3</a></li>
+					<li class="page-item"><a href="#" class="page-link">4</a></li>
+					<li class="page-item"><a href="#" class="page-link">5</a></li>
+					<li class="page-item"><a href="#" class="page-link">Next</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>        
+</div>
+
+
+<!-- Delete Modal HTML -->
+<div id="deleteEmployeeModal" class="modal fade" >
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form>
+				<div class="modal-header">						
+					<h4 class="modal-title">Delete Employee</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				</div>
+				<div class="modal-body">					
+					<p>Are you sure you want to delete these Records?</p>
+					<p class="text-warning"><small>This action cannot be undone.</small></p>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+					<!-- <input type="submit" class="btn btn-danger" value="Delete"> -->
+					<a href="<?php echo e($db_users->id); ?>" data-teacherid='<?php echo e($db_users->id); ?>' class="btn btn-danger">Delete</a>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+
+<!-- Edit Modal HTML -->
+<div id="addEmployeeModal" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form action="/index" method="POST" name="form" id="form" >
+			<?php echo csrf_field(); ?>
+				<div class="modal-header">						
+					<h4 class="modal-title">Add Employee</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				</div>
+						<input type="hidden" name="id" id="id" class="form-control" >
+				<div class="modal-body">					
+					<div class="form-group">
+						<label>Name</label>
+						<input type="text" name="username" id="username" class="form-control" value="<?php echo e(old('username')); ?>" >
+						<span id="nameErr"><?php $__errorArgs = ['username'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+					</div>
+					<div class="form-group">
+						<label>Email</label>
+						<input type="text" name="useremail" id="useremail" class="form-control" value="<?php echo e(old('useremail')); ?>" >
+						<span id="emailErr"><?php $__errorArgs = ['useremail'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+					</div>
+					<div class="form-group">
+						<label>Address</label>
+						<input type="text" class="form-control" name="useraddress" id="useraddress" value="<?php echo e(old('useraddress')); ?>" >
+						<span id="addressErr"><?php $__errorArgs = ['useraddress'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+					</div>
+					<div class="form-group">
+						<label>Phone</label>
+						<input type="text" name="userphone" id="userphone"  class="form-control" value="<?php echo e(old('userphone')); ?>" >
+						<span id="phoneErr"><?php $__errorArgs = ['userphone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?><?php echo e($message); ?><?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?></span>
+					</div>					
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-light" data-dismiss="modal" value="Cancel">
+					<input type="submit" class="btn btn-success" name="submit" id="submit" value="Add">
+					<!-- <input type="reset" class="btn btn-warning" name="submit" id="submit" value="Reset"> -->
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<script>
+	//Jquery Method to get the data of the row form the table
+	$(document).ready(function () {
+		$('.edit_user').click(function () { 
+			$tr = $(this).closest('tr');
+			var data = $tr.children("td").map(function() {
+				return $(this).text();
+			}).get();
+			// console.log(data);
+			$('#id').val(data[1]);
+			$('#username').val(data[2]);
+			$('#useremail').val(data[3]);
+			$('#useraddress').val(data[4]);
+			$('#userphone').val(data[5]);
+		});
+
+	});
+</script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\exp1\resources\views/index.blade.php ENDPATH**/ ?>
