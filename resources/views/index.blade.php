f38312b3679551b7116eef8b388d4d12c9801889
Muhammad Arslan <mughalarslan996@gmail.com>
2021-11-30 14:16:43 +0500

    Add files via upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,8 @@
 
 <!-- <link rel="stylesheet" href="../css/style.css"> -->
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
@@ -18,7 +20,9 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script src="{{asset('js/jqueryformvaldator.js')}}"></script>
+
+
+
 
 <script>
 $(document).ready(function(){
@@ -49,7 +53,24 @@ $(document).ready(function(){
 
 
 </head>
-<body><div class="container-xl">
+<body>
+@if (\Session::has('success'))
+	<div class="container mt-3">
+		<div class="alert alert-success alert-dismissible fade show" role="alert">
+			<strong>{!! \Session::get('success') !!} </strong>
+		</div>
+	</div>
+@endif
+
+@if(\Session::has('fail'))
+	<div class="container mt-3">
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+		<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+			<strong>{!! \Session::get('fail') !!} </strong>
+		</div>
+	</div>
+@endif
+	<div class="container-xl">
 	<div class="table-responsive">
 		<div class="table-wrapper">
 			<div class="table-title">
@@ -81,25 +102,33 @@ $(document).ready(function(){
 					</tr>
 				</thead>
 				<tbody>
-					@foreach($user as $db_users)
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox1" name="options[]" value="1">
-								<label for="checkbox1"></label>
-							</span>
-						</td>
-						<td>{{$db_users->id}}</td>
-						<td>{{$db_users->user_firstname}}</td>
-						<td>{{$db_users->user_email}}</td>
-						<td>{{$db_users->user_address}}</td>
-						<td>{{$db_users->user_phone}}</td>
-						<td>
-							<a href="{{$db_users->id}}" class="edit_user" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
- 							<a href="{{$db_users->id}}" class="delete" data-toggle="modal"  ><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>				
-					@endforeach
+					@if($user > 0)
+					$sr = 1;
+						@foreach($user as $db_users)
+						
+						<tr>
+							<td>
+								<span class="custom-checkbox">
+									<input type="checkbox" id="checkbox1" name="options[]" value="1">
+									<label for="checkbox1"></label>
+								</span>
+							</td>
+							<td>{{$db_users->id}}</td>
+							<td>{{$db_users->user_firstname}}</td>
+							<td>{{$db_users->user_email}}</td>
+							<td>{{$db_users->user_address}}</td>
+							<td>{{$db_users->user_phone}}</td>
+							<td>
+								<a href="{{$db_users->id}}" class="edit_user" ><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+								<a href="/delete_user/{{$db_users->id}}" class="delete" onclick="return confirm('* Are you sure to delete this record ?'); " ><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+							</td>
+						</tr>
+						@endforeach
+					@else
+						<tr>
+							<td colspan="7" class="text-center font-weight-bold bg-light">No Record Found </td>
+						</tr>
+					@endif	
 				</tbody>
 			</table>
 			<div class="clearfix">
@@ -135,7 +164,7 @@ $(document).ready(function(){
 				<div class="modal-footer">
 					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
 					<!-- <input type="submit" class="btn btn-danger" value="Delete"> -->
-					<a href="{{$db_users->id}}" data-teacherid='{{$db_users->id}}' class="btn btn-danger">Delete</a>
+					<button  data-teacherid='' class="btn btn-danger">Delete</button>
 				</div>
 			</form>
 		</div>
@@ -148,13 +177,13 @@ $(document).ready(function(){
 <div id="addEmployeeModal" class="modal fade">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<form action="/index" method="POST" name="form" id="form" >
+			<form action="/student" method="POST" name="form" id="form" >
 			@csrf
 				<div class="modal-header">						
 					<h4 class="modal-title">Add Employee</h4>
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 				</div>
-						<input type="hidden" name="id" id="id" class="form-control" >
+				<input type="hidden" name="id" id="id" class="form-control" >
 				<div class="modal-body">					
 					<div class="form-group">
 						<label>Name</label>
@@ -180,7 +209,6 @@ $(document).ready(function(){
 				<div class="modal-footer">
 					<input type="button" class="btn btn-light" data-dismiss="modal" value="Cancel">
 					<input type="submit" class="btn btn-success" name="submit" id="submit" value="Add">
-					<!-- <input type="reset" class="btn btn-warning" name="submit" id="submit" value="Reset"> -->
 				</div>
 			</form>
 		</div>
@@ -188,21 +216,41 @@ $(document).ready(function(){
 </div>
 <script>
 	//Jquery Method to get the data of the row form the table
-	$(document).ready(function () {
-		$('.edit_user').click(function () { 
-			$tr = $(this).closest('tr');
-			var data = $tr.children("td").map(function() {
-				return $(this).text();
-			}).get();
-			// console.log(data);
-			$('#id').val(data[1]);
-			$('#username').val(data[2]);
-			$('#useremail').val(data[3]);
-			$('#useraddress').val(data[4]);
-			$('#userphone').val(data[5]);
-		});
+	$(document).ready(function () 
+	{
+		$('.edit_user').click(function ()
+		{ 
+			var id = $(this).data("id");
+			console.log(id);
+			// $tr = $(this).closest('tr');
 
+			// var data = $tr.children("td").map(function() {
+			// 	return $(this).text();
+			// }).get();
+
+			// // console.log(data);
+			// var id = $('#id').val(data[1]);
+			// console.log(id);
+			// $('#username').val(data[2]);
+			// $('#useremail').val(data[3]);
+			// $('#useraddress').val(data[4]);
+			// $('#userphone').val(data[5]);
+		});
 	});
+
+	// //Delete Single user
+	// $(document).ready(function() {
+	// 	$('.delete').click(function() {
+	// 		if(confirm('* Are you sure to delete this record ?')){
+	// 			return true;
+	// 		}else{
+	// 			return false;
+	// 		}
+	// 	});
+	// });
+
 </script>
+<!-- <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script> -->
+<script src="{{asset('js/jqueryformvaldator.js')}}"></script>
 </body>
 </html>
