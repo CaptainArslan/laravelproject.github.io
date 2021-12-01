6cbeff4ded964ae15e66854543da7e3c731eb512
Muhammad Arslan <mughalarslan996@gmail.com>
2021-12-01 14:39:02 +0500

    Add files via upload

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -17,17 +17,38 @@ class UserModel extends Model
     function get_db_data()
     {
         $users = DB::select('select * from tbl_userdata');
-        return $users;
+        if( count($users) > 0){
+            return $users;
+        }
+    }
+    function get_student_data($id){
+        $get_users = DB::select("select * from tbl_userdata where id = '$id' ");
+            return $get_users;
+        
     }
 
     function db_insert($request){
-        $data = array();
         $insert = DB::table('tbl_userdata')->insert([
             'user_firstname' => $request->username,
             'user_email' => $request->useremail,
             'user_address' => $request->useraddress,
             'user_phone' => $request->userphone,
         ]);
+        if($insert){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
+    function db_delete($id){
+        $delete = DB::delete("DELETE FROM `tbl_userdata` WHERE `id` = '$id' ");
+        if($delete){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
