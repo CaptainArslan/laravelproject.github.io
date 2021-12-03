de0405ad55372eff89ff9378f935869f0a3f85f2
Muhammad Arslan <mughalarslan996@gmail.com>
2021-12-03 12:54:37 +0500

    insert update and ajax callling handling

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Facades\DB;
-
+use PHPUnit\Framework\Constraint\Count;
 
 class UserModel extends Model
 {
@@ -16,39 +16,103 @@ class UserModel extends Model
 
     function get_db_data()
     {
-        $users = DB::select('select * from tbl_userdata');
-        if( count($users) > 0){
+        $users = DB::select('SELECT * FROM `tbl_userdata`');
+        if( count($users) > 0)
+        {
             return $users;
         }
     }
-    function get_student_data($id){
-        $get_users = DB::select("select * from tbl_userdata where id = '$id' ");
-            return $get_users;
-        
+
+
+
+    function db_update ($request)
+    {
+        $duplicate = DB::select("SELECT user_email FROM `tbl_userdata` WHERE `user_email` = '$request->useremail' AND   `id` != '$request->id' ");
+        if(count($duplicate) > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            $update = DB::table('tbl_userdata') ->where('id', $request->id)
+            ->limit(1)
+            ->update([
+                'user_firstname' => $request->username,
+                'user_email' => $request->useremail,
+                'user_address' => $request->useraddress,
+                'user_phone' => $request->userphone,
+            ]);
+                if($update)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 2;
+                }
+        }
+        // var_dump($request->id);
+        // exit;
     }
 
-    function db_insert($request){
-        $insert = DB::table('tbl_userdata')->insert([
-            'user_firstname' => $request->username,
-            'user_email' => $request->useremail,
-            'user_address' => $request->useraddress,
-            'user_phone' => $request->userphone,
-        ]);
-        if($insert){
-            return true;
-        }
-        else{
-            return false;
-        }
+    function db_insert($request)
+    {
+        $duplicate = DB::select("SELECT user_email FROM `tbl_userdata` WHERE `user_email` = '$request->useremail' ");
+        $count = count($duplicate);
+            if($count > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                $insert = DB::table('tbl_userdata')->insert([
+                    'user_firstname' => $request->username,
+                    'user_email' => $request->useremail,
+                    'user_address' => $request->useraddress,
+                    'user_phone' => $request->userphone,
+                ]);
+                if($insert)
+                {
+                    return 0;
+                }
+                else{
+                    return 2;
+                }
+            }
+
     }
 
     function db_delete($id){
         $delete = DB::delete("DELETE FROM `tbl_userdata` WHERE `id` = '$id' ");
-        if($delete){
+        if($delete)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
+    
+
+
+    //Ajax Functions
+    function get_student_data($id){
+        $get_users = DB::select("SELECT * FROM `tbl_userdata` WHERE id = '$id' ");
+            return $get_users;
+    }
+
+    function get_email($email){
+        $get_email = DB::select("SELECT * FROM `tbl_userdata` WHERE user_email = '$email' ");
+        if (count($get_email) > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
 }
